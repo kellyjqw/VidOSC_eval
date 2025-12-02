5309052abf6c8c82cd98b33de07c6d6eb4f9b1d1
kellyjqw <kelly.jq.wang@gmail.com>
2025-12-02 13:44:15 -0600

    testing single_eval

--- single_eval.py
+++ single_eval.py
@@ -43,13 +43,7 @@ args = Namespace(
     transformer_layers=3,
     transformer_dim=512,
     transformer_dropout=0.1,
-    # training args
-    # gpus=1,
-    # lr=5e-4,
-    # wd=0.0001,
-    # batch_size=64,
-    # num_workers=8,
-    # n_epochs=50,
+
     log_dir='./logs',
     log_name='debug',
     ckpt="checkpoint/slicing.ckpt",
@@ -182,19 +176,60 @@ class Eval():
             json.dump(self.end_state_metrics, f, indent=2)
 
 
+class Models():
+    def __init__(self, checkpoint_dir="checkpoint") -> None:
+        self.models = {}
+        self.vocabs = {}
+        all_sc = ['rolling', 'squeezing', 'mashing', 'roasting', 'peeling', 'chopping', 'crushing', 
+            'melting', 'mincing', 'slicing','grating', 'sauteing', 'frying', 'blending', 'coating', 
+            'browning', 'grilling', 'shredding', 'whipping', 'zesting']
+        for sc in all_sc:
+            args = Namespace(
+                # data args
+                ann_dir='./data_files',
+                pseudolabel_dir='./videoclip_pseudolabel',
+                feat_dir='./data',
+                sc_list=[sc],
+                # model args
+                transformer_heads=4,
+                transformer_layers=3,
+                transformer_dim=512,
+                transformer_dropout=0.1,
+
+                log_dir='./logs',
+                log_name='debug',
+                ckpt=f"checkpoint/{sc}.ckpt",
+                use_gt_action=True,
+                det=1
+            )
+            task = FrameCls.load_from_checkpoint(checkpoint_path=f"checkpoint/{sc}.ckpt", args=args)
+            model = task.model
+            model.eval()
+            self.models[sc] = model
+            self.vocabs[sc] = task.vocab
+
+    def predict(self, osc, feat):
+        sc_name = osc.split("_")[0]
+        feat = feat.unsqueeze(0)
+        model = self.models[sc_name]
+        vocab = self.vocabs[sc_name]
+        pred = model(feat)
+        prob = torch.softmax(pred, dim=-1)
+        gt_category_id = vocab[sc_name]
+        st_prob = prob[:, [0, 3 * gt_category_id - 2, 3 * gt_category_id - 1, 3 * gt_category_id]]
+        return st_prob
+
+
+        
 
 def infer_state_idx(prob):
     pred_idx = torch.argmax(prob, dim=0).cpu().numpy()
     pred_idx = pred_idx[1:]
     return pred_idx
-# recommended simple way
-task = FrameCls.load_from_checkpoint(checkpoint_path="checkpoint/slicing.ckpt", args=args)
-model = task.model  # this is your FeatTimeTransformer
-vocab = task.vocab
 
-model.eval()
 test_dataset = HowToChangeFeatDataset(args)
 E = Eval(end_label=3, out_dir="vidOSC_results")
+M = Models()
 i = 0
 torch.set_printoptions(threshold=torch.inf, precision=2, sci_mode=False) 
 
@@ -205,14 +240,7 @@ with torch.no_grad():
 
         feat, label, osc, is_novel, video_id, video_name = batch
       
-        sc_name = osc.split("_")[0]
-        if sc_name != "slicing": continue
-        feat = feat.unsqueeze(0)
-        pred = model(feat)
-        prob = torch.softmax(pred, dim=-1)
-        gt_category_id = vocab[sc_name]
-        st_prob = prob[:, [0, 3 * gt_category_id - 2, 3 * gt_category_id - 1, 3 * gt_category_id]]
-
+        st_prob = M.predict(osc, feat)
         pred_4 = st_prob.argmax(dim=-1)  # (T,)
         gt_4 = label.view(-1).long()
 
